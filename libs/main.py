ac3562d2e2261c44dc00a22b5e4b7a7a3a919651
沈瑗杰 <3695888@qq.com>
2020-12-05 21:26:18 +0800

    hmmm not much change

--- libs/main.py
+++ libs/main.py
@@ -6,9 +6,6 @@ mail: 3695888@qq.com
 import pyglet
 import libs
 
-from pyglet.window import Window
-from pyglet.app import run
-
 
 class Game:
     def __init__(self):
@@ -21,11 +18,11 @@ class Game:
         # list
 
     def start_game(self):
-        run()
+        pyglet.app.run()
 
     def setup(self):
         # window
-        self.window = Window(width=800, height=600,
+        self.window = pyglet.window.Window(width=800, height=600,
                     caption='minecraft PE', resizable=True)
 
 
