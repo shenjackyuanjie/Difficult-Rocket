5766a18fd424d365fa046aa09ec29652e31432f7
沈瑗杰 <3695888@qq.com>
2020-12-21 21:48:52 +0800

    Physics Calculation！

--- libs/Game_threads/calculations.py
+++ libs/Game_threads/calculations.py
@@ -7,10 +7,18 @@ import libs
 import math
 
 
-def P_C(A, type, B):
+def P_C_M(A, B):  # stand for Physics Calculation multiple
     """
     formats:
-    type : "*" or "//"(or "/" "\" "\\")
+    A & B list format:docs.basic_config.json:basic_number
+    """
+    C = [0.0, 1, [], []]
+    return C
+
+
+def P_C_D(A, B):  # stand for Physics Calculation divide
+    """
+    formats:
     A & B list format:docs.basic_config.json:basic_number
     """
     C = [0.0, 1, [], []]
