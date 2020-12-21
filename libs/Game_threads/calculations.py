8090dd2fcfb13cf89cbee4961893115f50f240c9
沈瑗杰 <3695888@qq.com>
2020-12-21 21:59:36 +0800

    P_C_M！

--- libs/Game_threads/calculations.py
+++ libs/Game_threads/calculations.py
@@ -13,6 +13,17 @@ def P_C_M(A, B):  # stand for Physics Calculation multiple
     A & B list format:docs.basic_config.json:basic_number
     """
     C = [0.0, 1, [], []]
+    Float = A[0] * B[0]
+    Int = A[1] + B[1]
+    Unit1 = A[2].extend(B[2])
+    Unit2 = []
+    for unit in B[3]:
+        if unit in Unit1:
+            n = Unit1.index(unit)
+            Unit1.pop(n)
+        else:
+            Unit2.append(unit)
+    C = [Float, Int, Unit1, Unit2]
     return C
 
 
