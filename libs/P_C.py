6a02b2fa646589178cea112bbe15813ea68d8bbd
沈瑗杰 <3695888@qq.com>
2020-12-22 17:58:38 +0800

    Update P_C.py

--- libs/P_C.py
+++ libs/P_C.py
@@ -18,17 +18,12 @@ def P_C_M(A, B):  # stand for Physics Calculation multiple
     if Float >= 10:
         Float /= 10
         Int += 1
-    elif Float <= 0.01:
+    elif Float <= 1:
         Float *= 10
         Int -= 1
     else:
         pass
-    Unit1, Unit2 = A[2] + B[2], []
-    for unit in B[3]:
-        if unit in Unit1:
-            Unit1.remove(unit)
-        else:
-            Unit2.append(unit)
+    Unit1, Unit2 = A[2] + B[2], A[3] + B[3]
     if Unit1 == None:
         Unit1 = []
     for unit in Unit2:
@@ -36,7 +31,7 @@ def P_C_M(A, B):  # stand for Physics Calculation multiple
             Unit1.remove(unit)
             Unit2.remove(unit)
         else:
-            pass
+            continue
     C = [Float, Int, Unit1, Unit2]
     return C
 
@@ -46,7 +41,6 @@ def P_C_D(A, B):  # stand for Physics Calculation divide
     formats:
     A & B list format:docs.basic_config.json:basic_number
     """
-    B[2], B[3] = B[3], B[2]
     C = [0.0, 1, [], []]
     Float = float(A[0] / B[0])
     Int = int(A[1] - B[1])
@@ -58,12 +52,7 @@ def P_C_D(A, B):  # stand for Physics Calculation divide
         Int -= 1
     else:
         pass
-    Unit1, Unit2 = A[2] + B[2], []
-    for unit in B[3]:
-        if unit in Unit1:
-            Unit1.remove(unit)
-        else:
-            Unit2.append(unit)
+    Unit1, Unit2 = A[2] + B[3], A[3] + B[2]
     if Unit1 == None:
         Unit1 = []
     for unit in Unit2:
@@ -71,6 +60,6 @@ def P_C_D(A, B):  # stand for Physics Calculation divide
             Unit1.remove(unit)
             Unit2.remove(unit)
         else:
-            pass
+            continue
     C = [Float, Int, Unit1, Unit2]
     return C
