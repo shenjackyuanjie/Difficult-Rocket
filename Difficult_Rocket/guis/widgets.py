2a0d95eb8479dda7e3f0fe6bdb0c46f1661e542e
shenjackyuanjie <3695888@qq.com>
2021-11-06 14:17:47 +0800

    其实也没啥

--- Difficult_Rocket/guis/widgets.py
+++ Difficult_Rocket/guis/widgets.py
@@ -11,11 +11,14 @@ github: @shenjackyuanjie
 gitee:  @shenjackyuanjie
 """
 
-from ..api import translate
+from Difficult_Rocket import translate
 
 from libs import pyglet
+from libs.pyglet import font
+from libs.pyglet.text import Label
 from libs.pyglet.gui import widgets
 from libs.pyglet.sprite import Sprite
+from libs.pyglet.shapes import Rectangle
 from libs.pyglet.graphics import Batch, Group
 from libs.pyglet.image import AbstractImage
 
@@ -50,17 +53,48 @@ class InputBox(widgets.WidgetBase):
                  y: int,
                  width: int,
                  height: int,
-                 batch: Batch,
-                 group: Group,
-                 message: str = ''):
+                 message: str = '',
+                 font_name: str = translate.鸿蒙简体,
+                 font_size: int = 15,
+                 text_color: [int, int, int] = (0, 0, 0, 255),
+                 out_line_color: [int, int, int] = (255, 255, 255),
+
+                 out_line: int = 2,
+                 batch: Batch = Batch(),
+                 group: Group = Group()):
         super().__init__(x, y, width, height)
+        self._text = message
+        self.text = self._text
+        self.字体 = font.load(font_name, font_size)
+        self.字高 = self.字体.ascent - self.字体.descent
+        self.外框距离 = out_line
+        self._输入框 = Label(x=x + out_line, y=y + out_line,
+                          width=width, height=height,
+                          font_name=font_name, font_size=font_size,
+                          batch=batch, group=group,
+                          text=message)
+        self._外框 = Rectangle(x=x-out_line, y=y-out_line,
+                             width=width + (out_line * 2), height=height + (out_line * 2),
+                             batch=batch, group=group)
+        self._光标 = Rectangle(x=x+out_line, y=y+out_line,
+                             width=1, height=self.字高,
+                             batch=batch, group=group)
+
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, value):
+        assert type(value) is str, 'Input Box\'s text must be string!'
+        self._text = value
+        self._输入框.text = value
 
     @property
     def value(self):
-        return self.enabled
+        return self.text
 
     def _update_position(self):
-        pass
-
-a = InputBox(1, 2, 3, 4, 5, 6, 7)
-
+        self._输入框.position = self._x + self.外框距离, self._y + self.外框距离
+        self._外框.position = self._x - self.外框距离, self._y - self.外框距离
+        self._光标.position = self._x + self.外框距离, self._y + self.外框距离
