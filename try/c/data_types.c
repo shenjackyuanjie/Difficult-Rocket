bf96be1de23259ed1a58561a3002246b2dc5e5a1
沈瑗杰 <3695888@qq.com>
2022-04-30 11:33:04 +0800

    rua

--- try/c/data_types.c
+++ try/c/data_types.c
@@ -1,19 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
-// DR data types
 
-// 一些用于直观感受类型字节长度的数据类型
-// 看着舒服而已（
-#define int8 char
-#define int16 short
-#define int32 int
-#define int64 long long
-#define uint8 unsigned char
-#define uint16 unsigned short
-#define uint32 unsigned int
-#define uint64 unsigned long long
-#define float32 float
-#define float64 double
+#include"utils.h"
 
 struct part
 {
@@ -25,6 +11,6 @@ struct part
 int main(){
     long double a = 1.0;
     // 输出 a 的字节长度
-    printf("%o\n", sizeof(long double));
+    printf("%d\n", sizeof(long double));
 }
 
---
+++ try/c/utils.h
@@ -0,0 +1,19 @@
+// by shenjackyuanjie 
+
+// 基本的头文件引用
+#include<stdio.h>
+#include<stdlib.h>
+
+
+// 一些用于直观感受类型字节长度的数据类型
+// 看着舒服而已（
+#define int8 char
+#define int16 short
+#define int32 int
+#define int64 long long
+#define uint8 unsigned char
+#define uint16 unsigned short
+#define uint32 unsigned int
+#define uint64 unsigned long long
+#define float32 float
+#define float64 double
