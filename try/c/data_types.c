89aa0d421770912a84834c43cfc035cd4bbed904
沈瑗杰 <3695888@qq.com>
2022-04-30 12:57:32 +0800

    aaa

--- try/c/data_types.c
+++ try/c/data_types.c
@@ -1,16 +1,49 @@
+// DR数据类型
+#include <stdlib.h>
+#include "data_utils.h" // 自己搓的一个常用数据类型的头文件
 
-#include"utils.h"
-
-struct part
+struct ship_part
 {
     /* 一个 part 的数据格式 */
-    long double a;
-    uint16 b;
+    string part_type;
+    int32 part_id;
+    bool enabled;
+    float64 x;
+    float64 v_x;
+    float64 y;
+    float64 v_y;
+    float32 angle;
+    float64 v_angle;
+    bool flip_x;
+    bool flip_y;
+} ship_part = {
+    "pod",
+    0,
+    false,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    false,
+    false
 };
 
 int main(){
-    long double a = 1.0;
-    // 输出 a 的字节长度
-    printf("%d\n", sizeof(long double));
-}
+    // 创建一个 part
+    struct ship_part p;
+    p.part_type = "head";
+    p.part_id = 1;
+    p.enabled = true;
 
+    // 创建一个 part 的数组
+    struct ship_part *parts = malloc(sizeof(struct ship_part) * 10);
+
+    // 输出一个 part 的数据
+    printf("%s %d %d %f %f %f %f %f %f %d %d\n",
+        p.part_type, p.part_id, p.enabled,
+        p.x, p.v_x, p.y, p.v_y, p.angle, p.v_angle,
+        p.flip_x, p.flip_y);
+
+    return 0;
+}
--- try/c/data_utils.h
+++ try/c/data_utils.h
@@ -1,4 +1,4 @@
-// by shenjackyuanjie 
+/* by shenjackyuanjie  一些数据类型的转换 */
 
 // 基本的头文件引用
 #include<stdio.h>
@@ -7,13 +7,22 @@
 
 // 一些用于直观感受类型字节长度的数据类型
 // 看着舒服而已（
+// int 整数
 #define int8 char
 #define int16 short
 #define int32 int
 #define int64 long long
+// uint 无符号整数
 #define uint8 unsigned char
 #define uint16 unsigned short
 #define uint32 unsigned int
 #define uint64 unsigned long long
+// float 浮点数
 #define float32 float
 #define float64 double
+// bool 布尔值
+#define bool int8
+#define true 1
+#define false 0
+// 字符串
+#define string char*
