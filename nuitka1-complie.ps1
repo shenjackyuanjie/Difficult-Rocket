da4c9e8792d22ae6bad3d1914e38224bc9bae903
shenjack <3695888@qq.com>
2022-11-23 07:30:51 +0800

    compile script update

--- nuitka1-complie.ps1
+++ nuitka1-complie.ps1
@@ -1,11 +1,11 @@
 $start_time = Get-Uptime
 Write-Output $start_time
-python3.8.exe -m nuitka --jobs=24 --clang --lto=yes --mingw64 --show-memory --show-progress --output-dir=build/nuitka1 --include-data-dir=./configs=./configs --include-data-dir=./libs/fonts=./libs/fonts --include-data-dir=./textures=./textures --include-data-dir=./libs/pyglet=./pyglet --enable-plugin=numpy --nofollow-import-to=objprint,numpy,pillow,cffi,PIL,pyglet --standalone $args .\DR.py
+python3.8.exe -m nuitka --jobs=24 --clang --lto=no --mingw64 --show-memory --show-progress --output-dir=build/nuitka1 --include-data-dir=./configs=./configs --include-data-dir=./libs/fonts=./libs/fonts --include-data-dir=./textures=./textures --include-data-dir=./libs/pyglet=./pyglet --enable-plugin=numpy --nofollow-import-to=objprint,numpy,pillow,cffi,PIL,pyglet --standalone $args .\DR.py
 $end_time = Get-Uptime
 $out = $end_time.TotalMilliseconds - $start_time.TotalMilliseconds
 Write-Output $end_time.TotalSeconds $start_time.TotalSeconds $out s
 Write-Output $start_time $end_time
-Write-Output "--jobs=24 --clang --mingw64 --lto=yes and ($args)"
+Write-Output "--jobs=24 --clang --mingw64 --lto=no and ($args)"
 # --include-data-dir=./libs/pyglet=./pyglet
 # --run
 # --disable-ccache
