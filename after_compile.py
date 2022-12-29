50015f97ad8e93fa95b6a23c620d221cb2d4b704
shenjack <3695888@qq.com>
2022-12-29 12:14:51 +0800

    feat: improve compile

--- after_compile.py
+++ after_compile.py
@@ -11,6 +11,7 @@ print(os.listdir('./build'))
 
 with zipfile.ZipFile('./build/main.zip', 'w', zipfile.ZIP_DEFLATED, compresslevel=9) as dist_zip:
     for path, sub_paths, sub_files in os.walk('./build/DR.dist'):
+        print(f'writing {path}')
         for file in sub_files:
             file_path = os.path.join(path, file)
             dist_zip.write(file_path)
