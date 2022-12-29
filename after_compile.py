ff15d5ae98cf0f9c8defa75769df237e947f85f0
shenjack <3695888@qq.com>
2022-12-29 11:15:00 +0800

    feat: try some script

---
+++ after_compile.py
@@ -0,0 +1,8 @@
+#  -------------------------------
+#  Difficult Rocket
+#  Copyright © 2021-2022 by shenjackyuanjie 3695888@qq.com
+#  All rights reserved
+#  -------------------------------
+
+import os
+print(os.listdir('./build'))
