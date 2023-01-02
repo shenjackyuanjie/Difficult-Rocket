fb37486223421673b0640d42d19f5f4610cde493
shenjack <3695888@qq.com>
2023-01-02 19:22:22 +0800

    feat: try pwsh in Linux Macos CI

--- nuitka3-complie.ps1
+++ nuitka3-complie.ps1
@@ -1,6 +1,33 @@
 $start_time = Get-Uptime
 Write-Output $start_time
-python3.8.exe -m nuitka --jobs=24 --msvc=latest --clang --lto=no --show-memory --show-progress --output-dir=build/nuitka1 --include-data-dir=./configs=./configs --include-data-dir=./libs/fonts=./libs/fonts --include-data-dir=./textures=./textures --include-data-dir=./libs/pyglet=./pyglet --nofollow-import-to=objprint,numpy,pillow,cffi,PIL,pyglet --standalone $args .\DR.py
+
+$arg = @()
+# 输出配置
+$arg += @("--standalone")
+$arg += @("--output-dir=build/nuitka1")
+$arg += @("--company-name=tool-shenjack-workshop")
+$arg += @("--product-name=Difficult-Rocket")
+$arg += @("--product-version=0.6.4")
+$arg += @("--file-version=0.0.0.1")
+$arg += @("--file-description=Difficult-Rocket!")
+# 编译器配置
+$arg += @("--msvc=latest")
+$arg += @("--clang")
+$arg += @("--lto=no")
+# 包配置
+$arg += @("--nofollow-import-to=objprint,pillow,PIL,pyglet")
+# 数据配置
+$arg += @("--include-data-dir=./libs/pyglet=./pyglet")
+$arg += @("--include-data-dir=./libs/fonts=./libs/fonts")
+$arg += @("--include-data-dir=./textures=./textures")
+$arg += @("--include-data-dir=./configs=./configs")
+# 编译配置
+$arg += @("--jobs=24")
+$arg += @("--show-memory")
+$arg += @("--show-progress")
+$arg += @("--assume-yes-for-download")
+python3.8.exe -m nuitka $arg $args DR.py
+
 $end_time = Get-Uptime
 $out = $end_time.TotalMilliseconds - $start_time.TotalMilliseconds
 Write-Output $end_time.TotalSeconds $start_time.TotalSeconds $out s
