cc5840bef402547399833de704dda027a4440ce3
shenjack <3695888@qq.com>
2023-01-20 17:41:43 +0800

    \pyo3/
    nice rust

---
+++ libs/DR-rs/Difficult_Rocket_rs/__init__.pyi
@@ -0,0 +1,9 @@
+#  -------------------------------
+#  Difficult Rocket
+#  Copyright © 2021-2022 by shenjackyuanjie 3695888@qq.com
+#  All rights reserved
+#  -------------------------------
+
+def sum_as_string(a: int, b: int) -> int: ...
+
+def for_x_in_range(a: int, b: int) -> None: ...
--- libs/DR-rs/setup.py
+++ libs/DR-rs/setup.py
@@ -3,14 +3,16 @@
 #  Copyright © 2020-2023 by shenjackyuanjie 3695888@qq.com
 #  All rights reserved
 #  -------------------------------
-
+import sys
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
-    name='Difficult_Rocket-rs',
+    name='Difficult_Rocket_rs',
     version='0.0.0.1',
-    rust_extensions=[RustExtension("Difficult_Rocket-rs.Difficult_Rocket-rs", binding=Binding.PyO3)],
-    packages=['Difficult_Rocket-rs'],
+    rust_extensions=[RustExtension("Difficult_Rocket_rs.Difficult_Rocket_rs", binding=Binding.PyO3)],
+    packages=['Difficult_Rocket_rs'],
     zip_safe=False
 )
+
+print(sys.platform)
