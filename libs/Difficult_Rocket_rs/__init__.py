2d142b0ce32d3e4d0a023aa31a2e6342916b97c7
沈瑗杰 <3695888@qq.com>
2023-01-23 01:58:07 +0800

    [page skip]lots of things

--- libs/Difficult_Rocket_rs/__init__.py
+++ libs/Difficult_Rocket_rs/__init__.py
@@ -5,3 +5,15 @@
 #  -------------------------------
 
 from .lib import *
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from Difficult_Rocket.client.render.sr1_ship import SR1ShipRender, SR1ShipRender_Option
+    def sum_as_string(a: int, b: int) -> int: ...
+
+    def for_x_in_range(a: int, b: int) -> None: ...
+
+    def test_call(py_obj) -> bool: ...
+
+    def better_update_parts(render: SR1ShipRender, option: SR1ShipRender_Option) -> bool: ...
