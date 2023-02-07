43a4b138953a11dc759626d1be0dac6e5273a4b2
shenjack <3695888@qq.com>
2023-02-07 12:04:18 +0800

    这样也许行？

--- nuitka-win.ps1
+++ nuitka-win.ps1
@@ -53,7 +53,7 @@ $out = $end_time.TotalMilliseconds - $start_time.TotalMilliseconds
 Write-Output $end_time.TotalSeconds $start_time.TotalSeconds $out s
 Write-Output $start_time $end_time
 Write-Output "--clang --lto=no and $args"
-cp ./libs/pyglet ./build/nuitka-win/DR.dist/pyglet
+cp ./libs/pyglet ./build/nuitka-win/DR.dist
 # --include-data-dir=./libs/pyglet=./pyglet
 # --run
 # --disable-ccache
