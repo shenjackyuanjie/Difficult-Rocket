3bef1ead08fa63a0330e06dd1de21d29018c2f1b
shenjack <3695888@qq.com>
2023-03-23 00:40:12 +0800

    update pyglet_rs and DR_rs

--- nuitka-win.ps1
+++ nuitka-win.ps1
@@ -33,8 +33,8 @@ $arg += @("--msvc=latest")
 $arg += @("--clang")
 $arg += @("--lto=no")
 # 包配置
-$arg += @("--nofollow-import-to=objprint,pillow")
-$arg += @("--follow-import-to=pyglet")
+$arg += @("--nofollow-import-to=objprint,pillow,pyglet")
+#$arg += @("--follow-import-to=pyglet")
 # 数据配置
 # $arg += @("--include-data-dir=./libs/pyglet=./libs/pyglet")
 $arg += @("--include-data-dir=./libs/fonts=./libs/fonts")
