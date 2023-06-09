499c22e8cc1acbda7231caf8deec1fc916b5c022
shenjack <3695888@qq.com>
2023-06-09 15:10:15 +0800

    make build script using python

---
+++ nuitka_build.py
@@ -0,0 +1,13 @@
+#  -------------------------------
+#  Difficult Rocket
+#  Copyright © 2020-2023 by shenjackyuanjie 3695888@qq.com
+#  All rights reserved
+#  -------------------------------
+
+import subprocess
+from libs.utils import nuitka
+
+if __name__ == '__main__':
+    compiler = nuitka.Status()
+
+
