58796cd79c569a380d2f2e65d7944d30ef418c01
shenjack <3695888@qq.com>
2023-06-10 14:46:19 +0800

    update cargo dependency

--- mods/dr_game/Difficult_Rocket_rs/src/setup.py
+++ mods/dr_game/Difficult_Rocket_rs/src/setup.py
@@ -6,17 +6,18 @@
 import sys
 import shutil
 from setuptools import setup
-from setuptools_rust import Binding, RustExtension
+from setuptools_rust import Binding, RustExtension, Strip
 
 package_path = 'Difficult_Rocket_rs'
 
 setup(
     name='Difficult_Rocket_rs',
-    version="0.2.9.3",
+    version="0.2.10.1",
     author='shenjackyuanjie',
     author_email='3695888@qq.com',
     rust_extensions=[RustExtension(target="Difficult_Rocket_rs.Difficult_Rocket_rs",
-                                   binding=Binding.PyO3)],
+                                   binding=Binding.PyO3,
+                                   strip=Strip.All)],
     zip_safe=False,
 )
 
