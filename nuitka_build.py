a5ee30a534c701ef7be4011e3cf1422c64e05fc4
shenjack <3695888@qq.com>
2023-06-11 18:39:29 +0800

    remove useless double zip

--- nuitka_build.py
+++ nuitka_build.py
@@ -107,14 +107,6 @@ if __name__ == '__main__':
             except Exception:
                 traceback.print_exc()
             print('Remove Useless Files Done!')
-            # 压缩
-            with zipfile.ZipFile(Path('./build/Difficult_Rocket.zip'), 'w', zipfile.ZIP_DEFLATED, compresslevel=9) as dist_zip:
-                for path, sub_paths, sub_files in os.walk(compiler.output_path / 'DR.dist'):
-                    print(f'writing {path}')
-                    for file in sub_files:
-                        file_path = os.path.join(path, file)
-                        dist_zip.write(file_path)
-            print('Zip Done!')
         else:
             dist_dir_size = 0
             dist_file_size: Dict[str, Tuple[int, float]] = {}
