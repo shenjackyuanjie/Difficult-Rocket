0e7adcb3a0b0674e5c482230072230e95a26bce5
shenjack <3695888@qq.com>
2023-06-16 23:23:19 +0800

    [build skip] [build rs skip] rename+remove

--- Difficult_Rocket/__init__.py
+++ Difficult_Rocket/__init__.py
@@ -19,43 +19,21 @@ build_version = Version("2.1.0.0")  # 编译文件版本(与游戏本体无关)
 Api_version = Version("0.1.1.0")  # API 版本
 __version__ = game_version
 
-long_version: int = 15
-"""
-long_version: 一个用于标记内部协议的整数
-15: 完全移除 DR_rust 相关内容 解耦完成
-14: BaseScreen 的每一个函数都添加了一个参数: window: "ClientWindow"
-13: 为 DR_runtime 添加 API_version
-12: 去除 DR_runtime 的 global_logger
-    要 logging 自己拿去（
-11: 为 DR_option  添加 use_DR_rust
-    修复了一些拼写错误
-10: 为 DR_runtime 添加 DR_Rust_get_version
-9 : 为 DR_option  添加 pyglet_macosx_dev_test
-8 : 为 DR_runtime 添加 DR_rust_version
-    为 DR_option  添加 DR_rust_available
-    以后就有 DR_rust 了
-7 : 为 DR_option 添加 std_font_size
-6 : 事实证明, 不如直接用int
-5 : 添加 build_version 信息,用于标记编译文件版本,
-    游戏版本改为四位数，终于有一个可以让我随便刷的版本号位数了
-4 : 把 translate 的字体常量位置改了一下,顺便调换顺序
-3 : 就是试试改一下，正好 compiler 要用
-2 : 哦，对 longlong 好耶！
-1 : 我可算想起来还有这回事了 v0.6.4
-"""
 
-
-class _DR_option(Options):
+class _DR_status(Options):
     """
-    DR 的一般配置/状态
+    DR 的特性开关 / 基本状态
     """
     name = 'DR Option'
-    # runtime options
+    # run status
+    client_running: bool = False
+    server_running: bool = False
+
+    # feature switch
     InputBox_use_TextEntry:     bool = True
     record_threads:             bool = True
     report_translate_not_found: bool = True
     use_multiprocess:           bool = False
-    DR_rust_available:          bool = False
     use_cProfile:               bool = False
     use_local_logging:          bool = False
     
@@ -63,6 +41,16 @@ class _DR_option(Options):
     playing:                bool = False
     debugging:              bool = False
     crash_report_test:      bool = False
+
+    # game version status
+    DR_version: Version = game_version  # DR SDK 版本
+    Build_version: Version = build_version  # DR 构建 版本
+    API_version: Version = Api_version  # DR SDK API 版本
+
+    # game options
+    language: str = 'zh-CN'
+    mod_path: str = './mods'
+    default_language: str = 'zh-CN'
 
     # window option
     gui_scale: float = 1.0  # default 1.0 2.0 -> 2x 3 -> 3x
@@ -77,28 +65,13 @@ class _DR_runtime(Options):
     DR 的运行时配置/状态
     """
     name = 'DR Runtime'
-    # game version status
-    DR_version: Version = game_version  # DR SDK 版本
-    Build_version: Version = build_version  # DR 构建 版本
-
-    API_version: Version = Api_version  # DR SDK API 版本
-    DR_long_version: int = long_version  # DR SDK 内部协议版本 （不要问我为什么不用 Version，我也在考虑）
 
     DR_Mod_List: List[Tuple[str, Version]] = []  # DR Mod 列表 (name, version)
     
     # run status
-    running:               bool = False
     start_time_ns:         Optional[int] = None
     client_setup_cause_ns: Optional[int] = None
     server_setup_cause_ns: Optional[int] = None
-
-    # game runtimes
-    # global_logger: logging.Logger
-
-    # game options
-    mod_path: str = './mods'
-    language: str = 'zh-CN'
-    default_language: str = 'zh-CN'
 
     def load_file(self) -> bool:
         with contextlib.suppress(FileNotFoundError):
@@ -141,10 +114,10 @@ class _DR_runtime(Options):
         return mods
 
 
-DR_option = _DR_option()
+DR_status = _DR_status()
 DR_runtime = _DR_runtime()
 
-if DR_option.playing:
+if DR_status.playing:
     from Difficult_Rocket.utils.thread import new_thread
 
     def think_it(something):
--- docs/src/change_log/dr_sdk.md
+++ docs/src/change_log/dr_sdk.md
@@ -4,6 +4,64 @@
 - 最新版本号
   - DR sdk: 0.8.2.0
 
+## DR sdk 0.8.3.0
+
+### 添加
+
+- `DR_status`
+  - 实际上就是 `DR_runtime`
+  - `DR_runtime` -> `DR_status`
+  - `client_running`
+    - 客户端是否在运行
+    - Is client running
+  - `server_running`
+    - 服务器是否在运行
+    - Is server running
+
+### 移动
+
+- `Difficult_Rocket.DR_runtime`
+  - `DR_version` -> `DR_status.DR_version`
+  - `Build_version` -> `DR_status.Build_version`
+  - `API_version` -> `DR_status.API_version`
+  - `language` -> `DR_status.language`
+  - `mod_path` -> `DR_status.mod_path`
+
+### 删除
+
+- `Diffiuclt_Rocket.long_version`
+  - 不再使用整数标记版本号 (反正 `Version` 有一个完整的版本号比较机制)
+  - No longer use integer to mark version number (since `Version` has a complete version comparison mechanism)
+- `Difficult_Rocket.DR_rust_available`
+  - 似乎我忘记删掉这个 `DR_rs` 的耦合了
+
+```python
+long_version: int = 15
+"""
+long_version: 一个用于标记内部协议的整数
+15: 完全移除 DR_rust 相关内容 解耦完成
+14: BaseScreen 的每一个函数都添加了一个参数: window: "ClientWindow"
+13: 为 DR_runtime 添加 API_version
+12: 去除 DR_runtime 的 global_logger
+    要 logging 自己拿去（
+11: 为 DR_option  添加 use_DR_rust
+    修复了一些拼写错误
+10: 为 DR_runtime 添加 DR_Rust_get_version
+9 : 为 DR_option  添加 pyglet_macosx_dev_test
+8 : 为 DR_runtime 添加 DR_rust_version
+    为 DR_option  添加 DR_rust_available
+    以后就有 DR_rust 了
+7 : 为 DR_option 添加 std_font_size
+6 : 事实证明, 不如直接用int
+5 : 添加 build_version 信息,用于标记编译文件版本,
+    游戏版本改为四位数，终于有一个可以让我随便刷的版本号位数了
+4 : 把 translate 的字体常量位置改了一下,顺便调换顺序
+3 : 就是试试改一下，正好 compiler 要用
+2 : 哦，对 longlong 好耶！
+1 : 我可算想起来还有这回事了 v0.6.4
+"""
+```
+
 ## DR sdk 0.8.2.0
 
 ### Fix
