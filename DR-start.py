7792abc243e7106a579be2e80409d7ba6b978974
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-07-11 18:54:26 +0800

    update DR&DR-start.py

--- DR-start.py
+++ DR-start.py
@@ -7,6 +7,8 @@
 import os
 import sys
 import time
+import traceback
+import threading
 
 from pathlib import Path
 
@@ -31,22 +33,39 @@ def modify_path() -> None:
 
 
 def start(start_time_ns: int) -> None:
-    from Difficult_Rocket import main
+    from Difficult_Rocket import crash, DR_status
     from Difficult_Rocket.runtime import DR_runtime
+    from Difficult_Rocket.exception import TestError
     DR_runtime.start_time_ns = start_time_ns
-
     try:
+        from Difficult_Rocket import main
         main_game = main.Game()
-    except Exception as exp:
+        main_game.start()
+        if DR_status.crash_report_test:
+            raise TestError('debug crash test')
+    except:
+        trace = traceback.format_exc()
+        crash.create_crash_report(trace)
+        crash.write_info_to_cache(sys.stdout)
+    print(crash.all_thread)
+    print(crash.all_process)
+    for a_thread in threading.enumerate():
+        print(a_thread)
+        if a_thread.is_alive() and a_thread != threading.current_thread() and a_thread != threading.main_thread():
+            a_thread.join(2)  # wait for 2 sec
+    import pyglet
+    pyglet.app.exit()  # make sure that pyglet has stopped
 
 
-
-if __name__ == '__main__':
+def main() -> int:
     print(hi, f"\n{time.ctime()}")  # hi！
     # 记录启动信息
     start_time_ns = time.time_ns()
     print_path()
     modify_path()
     start(start_time_ns)
+    return 0
 
 
+if __name__ == '__main__':
+    sys.exit(main())
