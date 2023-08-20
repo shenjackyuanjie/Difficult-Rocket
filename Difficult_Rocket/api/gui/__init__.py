6dbffb322d2a6aad325fca44af54504d66d685c2
shenjack <3695888@qq.com>
2023-08-20 21:42:25 +0800

    improve widget

---
+++ Difficult_Rocket/api/gui/__init__.py
@@ -0,0 +1,9 @@
+#  -------------------------------
+#  Difficult Rocket
+#  Copyright © 2020-2023 by shenjackyuanjie 3695888@qq.com
+#  All rights reserved
+#  -------------------------------
+
+__all__ = [
+    'widget'
+]
---
+++ Difficult_Rocket/api/gui/widget/__init__.py
@@ -0,0 +1,11 @@
+#  -------------------------------
+#  Difficult Rocket
+#  Copyright © 2020-2023 by shenjackyuanjie 3695888@qq.com
+#  All rights reserved
+#  -------------------------------
+
+from Difficult_Rocket.gui.widget.button import PressTextButton
+
+__all__ = [
+    'PressTextButton'
+]
