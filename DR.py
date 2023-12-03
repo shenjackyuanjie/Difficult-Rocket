5361c108686ec950753e9d6983c308d241eaaaa6
shenjack <3695888@qq.com>
2023-12-03 16:53:57 +0800

    ruff check

--- DR.py
+++ DR.py
@@ -18,31 +18,33 @@ QQ: 3695888"""
 
 
 def print_path() -> None:
-    print(f'{__file__=}')
-    print(f'{sys.path=}')
-    print(f'{sys.path[0]=}')
-    print(f'{sys.argv[0]=}')
-    print(f'{Path.cwd()=}')
-    print(f'{Path(__file__).absolute()=}')
-    print(f'{Path(__file__).parent=}')
+    print(f"{__file__=}")
+    print(f"{sys.path=}")
+    print(f"{sys.path[0]=}")
+    print(f"{sys.argv[0]=}")
+    print(f"{Path.cwd()=}")
+    print(f"{Path(__file__).absolute()=}")
+    print(f"{Path(__file__).parent=}")
 
 
 def modify_path() -> None:
     os.chdir(Path(__file__).parent)  # 将运行路径切换到文件位置 防止bug
-    sys.path.append('./libs')  # 添加 libs path
+    sys.path.append("./libs")  # 添加 libs path
 
 
 def start(start_time_ns: int) -> None:
     from Difficult_Rocket import crash, DR_status
     from Difficult_Rocket.runtime import DR_runtime
     from Difficult_Rocket.exception import TestError
+
     DR_runtime.start_time_ns = start_time_ns
     try:
         from Difficult_Rocket import main
+
         main_game = main.Game()
         main_game.start()
         if DR_status.crash_report_test:
-            raise TestError('debug crash test')
+            raise TestError("debug crash test")
     except:  # noqa: E722
         trace = traceback.format_exc()
         crash.create_crash_report(trace)
@@ -53,9 +55,13 @@ def start(start_time_ns: int) -> None:
     for a_thread in threading.enumerate():
         print(a_thread)
         if a_thread.is_alive() and not a_thread.daemon:
-            if a_thread != threading.current_thread() and a_thread != threading.main_thread():
+            if (
+                a_thread != threading.current_thread()
+                and a_thread != threading.main_thread()
+            ):
                 a_thread.join(2)  # wait for 2 sec
     import pyglet
+
     pyglet.app.exit()  # make sure that pyglet has stopped
 
 
@@ -69,5 +75,5 @@ def main() -> int:
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
